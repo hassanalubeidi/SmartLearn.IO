219c0e27815ab22e53cae21840c7cccb45877849
Prelang Builder <builder@prelang.com>
2014-11-02 00:07:42 +0000

    Overrode Devise's find_first_by_auth_conditions to allow finding a User by their username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :login
+  
+  #->Prelang (user_login:devise/username_login_support)
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.downcase}]).first
+    else
+      where(conditions).first
+    end
+  end
+
+
 end
