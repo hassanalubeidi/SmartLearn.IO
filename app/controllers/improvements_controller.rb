7ff2d640f14aa11847d40c082bd1966b7499eb68
Prelang Builder <builder@prelang.com>
2014-11-02 00:08:02 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/improvements_controller.rb
+++ app/controllers/improvements_controller.rb
@@ -1,4 +1,8 @@
 class ImprovementsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_improvement, only: [:show, :edit, :update, :destroy]
 
   # GET /improvements
