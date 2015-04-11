e0f1b6d1d6624abcdb4f0e8b2bae64fe7afff92b
hassanalubeidi <YOUR@EMAIL.com>
2015-04-11 23:22:12 +0100

    updated subject show page to include testpapers

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,7 @@ class Subject < ActiveRecord::Base
 	belongs_to :user
 	has_many :improvements, :dependent => :destroy
 	has_many :topics, :dependent => :destroy
+	has_many :test_papers
 
 	validates :name, presence: true
 end
