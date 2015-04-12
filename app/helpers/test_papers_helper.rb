01929b048e9d138fd7f4e8b6e9ff325ab04e3f09
hassanalubeidi <hassanalubeidi1996@gmail.com>
2015-04-12 20:44:28 +0100

    all marks??????

--- app/helpers/test_papers_helper.rb
+++ app/helpers/test_papers_helper.rb
@@ -2,7 +2,11 @@ module TestPapersHelper
 	def total_marks(test_paper)
 		all_marks = 0
 		test_paper.questions.each do |question|
-			ans = question.answers.where(:user => current_user).last.marks_integer
+			if question.answers.where(question: question, user: current_user).count != 0 then
+				ans = question.answers.where(:user => current_user).last.marks_integer
+			else
+				ans = 0
+			end
 			all_marks += ans
 		end
 		return all_marks
