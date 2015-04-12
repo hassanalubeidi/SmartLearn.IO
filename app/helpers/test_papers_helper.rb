216262091b3a1bb6147832434c2c574e1e431df6
hassanalubeidi <hassanalubeidi1996@gmail.com>
2015-04-12 20:40:35 +0100

    all marks???

--- app/helpers/test_papers_helper.rb
+++ app/helpers/test_papers_helper.rb
@@ -1,2 +1,10 @@
 module TestPapersHelper
+	def total_marks(test_paper)
+		all_marks = 0
+		test_paper.questions.each do |question|
+			ans = question.answers.where(:user => current_user).last.marks_integer
+			all_marks += ans
+		end
+		return all_marks
+	end
 end
