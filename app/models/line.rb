6a0d677a99d95f6e4a091ecdb72a8ea53b50f38f
hassanalubeidi <hassanalubeidi1996@gmail.com>
2015-04-19 20:33:03 +0100

    added destroy dependencies for some stuff

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,4 @@
 class Line < ActiveRecord::Base
-  belongs_to :question
-  belongs_to :objective
+  belongs_to :question, :dependent => :destroy
+  belongs_to :objective, :dependent => :destroy
 end
--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,6 +1,6 @@
 class Objective < ActiveRecord::Base
   has_many :lines
-  has_many :questions, through: :lines
-  has_many :objective_states
+  has_many :questions, through: :lines, :dependent => :destroy
+  has_many :objective_states, :dependent => :destroy
   belongs_to :topic
 end
